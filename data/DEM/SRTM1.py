182c77aefb778c876e52807a7e6884219487f28a
Michal Migurski <migurski@domU-12-31-39-09-1D-E8.compute-1.internal>
2011-05-22 00:47:41 +0000

    Converted DEM to a module

---
+++ data/DEM/SRTM1.py
@@ -0,0 +1,150 @@
+""" Starting point for DEM retrieval utilities.
+"""
+from sys import stderr
+from math import floor
+from os import unlink, close, write, mkdir, chmod
+from os.path import basename, exists, isdir, join
+from httplib import HTTPConnection
+from urlparse import urlparse
+from tempfile import mkstemp
+from zipfile import ZipFile
+from hashlib import md5
+
+from TileStache.Geography import SphericalMercator
+
+from osgeo import gdal, osr
+
+source_dir = 'source'
+
+osr.UseExceptions() # <-- otherwise errors will be silent and useless.
+
+sref = osr.SpatialReference()
+sref.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+
+def region(lat, lon):
+    """ Return the SRTM1 region number of a given lat, lon.
+    
+        Map of regions:
+        http://dds.cr.usgs.gov/srtm/version2_1/SRTM1/Region_definition.jpg
+    """
+    if 38 <= lat and lat < 50 and -125 <= lon and lon < -111:
+        return 1
+    
+    if 38 <= lat and lat < 50 and -111 <= lon and lon < -97:
+        return 2
+    
+    if 38 <= lat and lat < 50 and -97 <= lon and lon < -83:
+        return 3
+    
+    if 28 <= lat and lat < 38 and -123 <= lon and lon < -100:
+        return 4
+    
+    if 25 <= lat and lat < 38 and -100 <= lon and lon < -83:
+        return 5
+    
+    if 17 <= lat and lat < 48 and -83 <= lon and lon < -64:
+        return 6
+    
+    if -15 <= lat and lat < 60 and ((172 <= lon and lon < 180) or (-180 <= lon and lon < -129)):
+        return 7
+    
+    raise ValueError('Unknown location: %s, %s' % (lat, lon))
+
+def quads(minlon, minlat, maxlon, maxlat):
+    """ Generate a list of southwest (lon, lat) for 1-degree quads of SRTM1 data.
+    """
+    lon = floor(minlon)
+    while lon <= maxlon:
+    
+        lat = floor(minlat)
+        while lat <= maxlat:
+        
+            yield lon, lat
+        
+            lat += 1
+    
+        lon += 1
+
+def datasource(lat, lon):
+    """ Return a gdal datasource for an SRTM1 lat, lon corner.
+    
+        If it doesn't already exist locally in source_dir, grab a new one.
+    """
+    #
+    # Create a URL
+    #
+    try:
+        reg = region(lat, lon)
+    except ValueError:
+        # we're probably outside a known region
+        return None
+
+    fmt = 'http://dds.cr.usgs.gov/srtm/version2_1/SRTM1/Region_%02d/N%dW%d.hgt.zip'
+    url = fmt % (reg, abs(lat), abs(lon))
+    
+    #
+    # Create a local filepath
+    #
+    s, host, path, p, q, f = urlparse(url)
+    
+    dem_dir = md5(url).hexdigest()[:2]
+    dem_dir = join(source_dir, dem_dir)
+    
+    dem_path = join(dem_dir, basename(path)[:-4])
+    
+    #
+    # Check if the file exists locally
+    #
+    if exists(dem_path):
+        return gdal.Open(dem_path, gdal.GA_ReadOnly)
+
+    if not exists(dem_dir):
+        mkdir(dem_dir)
+        chmod(dem_dir, 0777)
+    
+    assert isdir(dem_dir)
+    
+    #
+    # Grab a fresh remote copy
+    #
+    print >> stderr, 'Retrieving', url, 'in DEM.SRTM1.datasource().'
+    
+    conn = HTTPConnection(host, 80)
+    conn.request('GET', path)
+    resp = conn.getresponse()
+    
+    assert resp.status == 200, (resp.status, resp.read())
+    
+    try:
+        #
+        # Get the DEM out of the zip file
+        #
+        handle, zip_path = mkstemp(prefix='srtm1-', suffix='.zip')
+        write(handle, resp.read())
+        close(handle)
+        
+        zipfile = ZipFile(zip_path, 'r')
+        
+        #
+        # Write the actual DEM
+        #
+        dem_file = open(dem_path, 'w')
+        dem_file.write(zipfile.read(zipfile.namelist()[0]))
+        dem_file.close()
+        
+        chmod(dem_path, 0666)
+    
+    finally:
+        unlink(zip_path)
+
+    #
+    # The file better exist locally now
+    #
+    return gdal.Open(dem_path, gdal.GA_ReadOnly)
+
+def datasources(minlon, minlat, maxlon, maxlat):
+    """ Retrieve a list of SRTM1 datasources overlapping the tile coordinate.
+    """
+    lonlats = quads(minlon, minlat, maxlon, maxlat)
+    sources = [datasource(lat, lon) for (lon, lat) in lonlats]
+    return [ds for ds in sources if ds]
--- data/DEM/__init__.py
+++ data/DEM/__init__.py
@@ -1,15 +1,11 @@
 """ Starting point for DEM retrieval utilities.
 """
-from sys import stderr
-from math import floor, pi, sin, cos
-from os import unlink, close, write, mkdir, chmod
-from os.path import basename, exists, isdir, join
-from httplib import HTTPConnection
+from math import pi, sin, cos
+from os import unlink, close
 from itertools import product
-from urlparse import urlparse
 from tempfile import mkstemp
-from zipfile import ZipFile
-from hashlib import md5
+
+import SRTM1
 
 from ModestMaps.Core import Coordinate
 from TileStache.Geography import SphericalMercator
@@ -19,17 +15,11 @@ from PIL import Image
 
 import numpy
 
-source_dir = 'source'
-pixel_buffer = 1
-
 #
 # Set up some useful projections.
 #
 
 osr.UseExceptions() # <-- otherwise errors will be silent and useless.
-
-srtm1_sref = osr.SpatialReference()
-srtm1_sref.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
 
 webmerc_proj = SphericalMercator()
 webmerc_sref = osr.SpatialReference()
@@ -71,12 +61,12 @@ class Provider:
         # Reproject and merge DEM datasources into the destination dataset.
         #
         
-        cs2cs = osr.CoordinateTransformation(webmerc_sref, srtm1_sref)
+        cs2cs = osr.CoordinateTransformation(webmerc_sref, SRTM1.sref)
         
         minlon, minlat, z = cs2cs.TransformPoint(xmin, ymin)
         maxlon, maxlat, z = cs2cs.TransformPoint(xmax, ymax)
         
-        for ds_in in srtm1_datasources(minlon, minlat, maxlon, maxlat):
+        for ds_in in SRTM1.datasources(minlon, minlat, maxlon, maxlat):
             gdal.ReprojectImage(ds_in, ds_elevation, ds_in.GetProjection(), ds_elevation.GetProjection(), gdal.GRA_Cubic)
             ds_in.FlushCache()
         
@@ -139,127 +129,6 @@ class SlopeAndAspect:
         finally:
             unlink(filename)
 
-def srtm1_region(lat, lon):
-    """ Return the SRTM1 region number of a given lat, lon.
-    
-        Map of regions:
-        http://dds.cr.usgs.gov/srtm/version2_1/SRTM1/Region_definition.jpg
-    """
-    if 38 <= lat and lat < 50 and -125 <= lon and lon < -111:
-        return 1
-    
-    if 38 <= lat and lat < 50 and -111 <= lon and lon < -97:
-        return 2
-    
-    if 38 <= lat and lat < 50 and -97 <= lon and lon < -83:
-        return 3
-    
-    if 28 <= lat and lat < 38 and -123 <= lon and lon < -100:
-        return 4
-    
-    if 25 <= lat and lat < 38 and -100 <= lon and lon < -83:
-        return 5
-    
-    if 17 <= lat and lat < 48 and -83 <= lon and lon < -64:
-        return 6
-    
-    if -15 <= lat and lat < 60 and ((172 <= lon and lon < 180) or (-180 <= lon and lon < -129)):
-        return 7
-    
-    raise ValueError('Unknown location: %s, %s' % (lat, lon))
-
-def srtm1_quads(minlon, minlat, maxlon, maxlat):
-    """ Generate a list of southwest (lon, lat) for 1-degree quads of SRTM1 data.
-    """
-    lon = floor(minlon)
-    while lon <= maxlon:
-    
-        lat = floor(minlat)
-        while lat <= maxlat:
-        
-            yield lon, lat
-        
-            lat += 1
-    
-        lon += 1
-
-def srtm1_datasource(lat, lon):
-    """ Return a gdal datasource for an SRTM1 lat, lon corner.
-    
-        If it doesn't already exist locally in source_dir, grab a new one.
-    """
-    #
-    # Create a URL
-    #
-    try:
-        reg = srtm1_region(lat, lon)
-    except ValueError:
-        # we're probably outside a known region
-        return None
-
-    fmt = 'http://dds.cr.usgs.gov/srtm/version2_1/SRTM1/Region_%02d/N%dW%d.hgt.zip'
-    url = fmt % (reg, abs(lat), abs(lon))
-    
-    #
-    # Create a local filepath
-    #
-    s, host, path, p, q, f = urlparse(url)
-    
-    dem_dir = md5(url).hexdigest()[:2]
-    dem_dir = join(source_dir, dem_dir)
-    
-    dem_path = join(dem_dir, basename(path)[:-4])
-    
-    #
-    # Check if the file exists locally
-    #
-    if exists(dem_path):
-        return gdal.Open(dem_path, gdal.GA_ReadOnly)
-
-    if not exists(dem_dir):
-        mkdir(dem_dir)
-        chmod(dem_dir, 0777)
-    
-    assert isdir(dem_dir)
-    
-    #
-    # Grab a fresh remote copy
-    #
-    print >> stderr, 'Retrieving', url, 'in DEM.srtm1_datasource().'
-    
-    conn = HTTPConnection(host, 80)
-    conn.request('GET', path)
-    resp = conn.getresponse()
-    
-    assert resp.status == 200, (resp.status, resp.read())
-    
-    try:
-        #
-        # Get the DEM out of the zip file
-        #
-        handle, zip_path = mkstemp(prefix='srtm1-', suffix='.zip')
-        write(handle, resp.read())
-        close(handle)
-        
-        zipfile = ZipFile(zip_path, 'r')
-        
-        #
-        # Write the actual DEM
-        #
-        dem_file = open(dem_path, 'w')
-        dem_file.write(zipfile.read(zipfile.namelist()[0]))
-        dem_file.close()
-        
-        chmod(dem_path, 0666)
-    
-    finally:
-        unlink(zip_path)
-
-    #
-    # The file better exist locally now
-    #
-    return gdal.Open(dem_path, gdal.GA_ReadOnly)
-
 def tile_bounds(coord, sref, buffer=0):
     """ Retrieve bounding box of a tile coordinate in specified projection.
     
@@ -276,13 +145,6 @@ def tile_bounds(coord, sref, buffer=0):
     xmax, ymin, z = cs2cs.TransformPoint(lr.x, lr.y)
     
     return xmin, ymin, xmax, ymax
-
-def srtm1_datasources(minlon, minlat, maxlon, maxlat):
-    """ Retrieve a list of SRTM1 datasources overlapping the tile coordinate.
-    """
-    quads = srtm1_quads(minlon, minlat, maxlon, maxlat)
-    sources = [srtm1_datasource(lat, lon) for (lon, lat) in quads]
-    return [ds for ds in sources if ds]
 
 def slope2bytes(slope):
     """ Convert slope from floating point to 8-bit.
@@ -336,40 +198,3 @@ def calculate_slope_aspect(elevation, xres, yres, z=1.0):
     aspect = numpy.arctan2(x, y)
     
     return slope, aspect
-
-if __name__ == '__main__':
-
-    provider = Provider(None)
-    coord = Coordinate(1582, 659, 12)
-    
-    xmin, ymin, xmax, ymax = tile_bounds(coord, webmerc_sref)
-    
-    image = provider.renderArea(256, 256, webmerc_proj.srs, xmin, ymin, xmax, ymax, coord.zoom)
-    image.save(open('both.tif', 'w'), 'TIFF')
-    
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    
-    print 'calculating shade...'
-    
-    azimuth, altitude = 315.0, 45.0
-    
-    ds_both = gdal.Open('both.tif')
-
-    band_slope = ds_both.GetRasterBand(1)
-    slope = bytes2slope(band_slope.ReadAsArray())
-    
-    band_aspect = ds_both.GetRasterBand(2)
-    aspect = bytes2aspect(band_aspect.ReadAsArray())
-    
-    deg2rad = pi / 180.0
-    
-    shaded = sin(altitude * deg2rad) * numpy.sin(slope) \
-           + cos(altitude * deg2rad) * numpy.cos(slope) \
-           * numpy.cos((azimuth - 90.0) * deg2rad - aspect);
-    
-    shaded_data = (0xFF * shaded).astype(numpy.uint8)
-
-    driver = gdal.GetDriverByName('GTiff')
-    ds_shaded = driver.Create('shaded-j95.tif', 256, 256, 1, gdal.GDT_Byte)
-    ds_shaded.WriteRaster(0, 0, shaded.shape[0], shaded.shape[1], shaded_data.tostring(), buf_type=gdal.GDT_Byte)
-    ds_shaded.FlushCache()
