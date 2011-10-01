9edf9c3fd50518c60b91bd59cfa626d70c785ebc
Michal Migurski <migurski@domU-12-31-39-09-1D-E8.compute-1.internal>
2011-10-01 21:19:53 +0000

    Pulled some of data up into Hillup.data

---
+++ Hillup/data/NED10m.py
@@ -0,0 +1,143 @@
+""" Starting point for DEM retrieval utilities.
+"""
+from sys import stderr
+from math import floor, ceil, log
+from os import unlink, close, write, mkdir, chmod
+from os.path import basename, exists, isdir, join
+from tempfile import mkstemp, mkdtemp
+from httplib import HTTPConnection
+from shutil import move, rmtree
+from urlparse import urlparse
+from zipfile import ZipFile
+from fnmatch import fnmatch
+from hashlib import md5
+
+from TileStache.Geography import SphericalMercator
+
+from osgeo import gdal, osr
+
+ideal_zoom = 15 ### log(3 * 3600*360 / 256) / log(2) # ~13.9
+
+osr.UseExceptions() # <-- otherwise errors will be silent and useless.
+
+sref = osr.SpatialReference()
+sref.ImportFromProj4('+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs')
+
+def quads(minlon, minlat, maxlon, maxlat):
+    """ Generate a list of northwest (lon, lat) for 1-degree quads of NED 10m data.
+    """
+    lon = floor(minlon)
+    while lon <= maxlon:
+    
+        lat = ceil(maxlat)
+        while lat >= minlat:
+        
+            yield lon, lat
+        
+            lat -= 1
+    
+        lon += 1
+
+def datasource(lat, lon, source_dir):
+    """ Return a gdal datasource for a NED 10m lat, lon corner.
+    
+        If it doesn't already exist locally in source_dir, grab a new one.
+    """
+    #
+    # Create a URL - tdds.cr.usgs.gov looks to be a redirect from
+    # http://gisdata.usgs.gov/TDDS/DownloadFile.php?TYPE=ned3f_zip&FNAME=nxxwxx.zip
+    #
+    # FIXME for southern/western hemispheres
+    fmt = 'http://tdds.cr.usgs.gov/ned/13arcsec/float/float_zips/n%02dw%03d.zip'
+    url = fmt % (abs(lat), abs(lon))
+    
+    #
+    # Create a local filepath
+    #
+    s, host, path, p, q, f = urlparse(url)
+    
+    local_dir = md5(url).hexdigest()[:3]
+    local_dir = join(source_dir, local_dir)
+    
+    local_base = join(local_dir, basename(path)[:-4])
+    local_path = local_base + '.flt'
+    local_none = local_base + '.404'
+    
+    #
+    # Check if the file exists locally
+    #
+    if exists(local_path):
+        return gdal.Open(local_path, gdal.GA_ReadOnly)
+
+    if exists(local_none):
+        return None
+
+    if not exists(local_dir):
+        mkdir(local_dir)
+        chmod(local_dir, 0777)
+    
+    assert isdir(local_dir)
+    
+    #
+    # Grab a fresh remote copy
+    #
+    print >> stderr, 'Retrieving', url, 'in DEM.NED10m.datasource().'
+    
+    conn = HTTPConnection(host, 80)
+    conn.request('GET', path)
+    resp = conn.getresponse()
+    
+    if resp.status == 404:
+        # we're probably outside the coverage area
+        print >> open(local_none, 'w'), url
+        return None
+    
+    assert resp.status == 200, (resp.status, resp.read())
+    
+    try:
+        dirpath = mkdtemp(prefix='ned10m-')
+        zippath = join(dirpath, 'dem.zip')
+
+        zipfile = open(zippath, 'w')
+        zipfile.write(resp.read())
+        zipfile.close()
+
+        zipfile = ZipFile(zippath)
+        
+        for name in zipfile.namelist():
+            if fnmatch(name, '*/*/float*.???') and name[-4:] in ('.hdr', '.flt', '.prj'):
+                local_file = local_base + name[-4:]
+
+            elif fnmatch(name, '*/float*_13.???') and name[-4:] in ('.hdr', '.flt', '.prj'):
+                local_file = local_base + name[-4:]
+
+            elif fnmatch(name, '*/float*_13'):
+                local_file = local_base + '.flt'
+
+            else:
+                # don't recognize the contents of this zip file
+                continue
+            
+            zipfile.extract(name, dirpath)
+            move(join(dirpath, name), local_file)
+            
+            if local_file.endswith('.hdr'):
+                # GDAL needs some extra hints to understand the raw float data
+                hdr_file = open(local_file, 'a')
+                print >> hdr_file, 'nbits 32'
+                print >> hdr_file, 'pixeltype float'
+        
+        #
+        # The file better exist locally now
+        #
+        return gdal.Open(local_path, gdal.GA_ReadOnly)
+    
+    finally:
+        rmtree(dirpath)
+
+def datasources(minlon, minlat, maxlon, maxlat, source_dir):
+    """ Retrieve a list of SRTM1 datasources overlapping the tile coordinate.
+    """
+    lonlats = quads(minlon, minlat, maxlon, maxlat)
+    sources = [datasource(lat, lon, source_dir) for (lon, lat) in lonlats]
+    return [ds for ds in sources if ds]
---
+++ Hillup/data/__init__.py
@@ -0,0 +1,269 @@
+""" Starting point for DEM retrieval utilities.
+"""
+from math import pi, sin, cos
+from os import unlink, close
+from itertools import product
+from tempfile import mkstemp
+
+import NED10m, SRTM1, SRTM3
+
+from ModestMaps.Core import Coordinate
+from TileStache.Geography import SphericalMercator
+
+from osgeo import gdal, osr
+from PIL import Image
+
+import numpy
+
+# used to prevent clobbering in /vsimem/, see:
+# http://osgeo-org.1803224.n2.nabble.com/gdal-dev-Outputting-to-vsimem-td6221295.html
+vsimem_counter = 1
+
+#
+# Set up some useful projections.
+#
+
+osr.UseExceptions() # <-- otherwise errors will be silent and useless.
+
+webmerc_proj = SphericalMercator()
+webmerc_sref = osr.SpatialReference()
+webmerc_sref.ImportFromProj4(webmerc_proj.srs)
+
+class Provider:
+    """ TileStache provider for generating tiles of DEM slope and aspect data.
+
+        See http://tilestache.org/doc/#custom-providers for information
+        on how the Provider object interacts with TileStache.
+    """
+    def __init__(self, layer, source_dir, tmpdir=None):
+        self.tmpdir = tmpdir
+        self.source_dir = source_dir
+    
+    def getTypeByExtension(self, ext):
+        if ext.lower() != 'tiff':
+            raise Exception()
+        
+        return 'image/tiff', 'TIFF'
+    
+    def renderArea(self, width, height, srs, xmin, ymin, xmax, ymax, zoom):
+        """ Return an instance of SlopeAndAspect for requested area.
+        """
+        assert srs == webmerc_proj.srs # <-- good enough for now
+        
+        #
+        # Prepare information for datasets of the desired extent and projection.
+        #
+        
+        xres = (xmax - xmin) / width
+        yres = (ymin - ymax) / height
+
+        area_wkt = webmerc_sref.ExportToWkt()
+        buffered_xform = xmin - xres, xres, 0, ymax - yres, 0, yres
+        
+        #
+        # Reproject and merge DEM datasources into destination datasets.
+        #
+        
+        driver = gdal.GetDriverByName('GTiff')
+        elevation = numpy.zeros((width+2, height+2), numpy.float32)
+        
+        for (module, proportion) in choose_providers(zoom):
+        
+            cs2cs = osr.CoordinateTransformation(webmerc_sref, module.sref)
+            
+            minlon, minlat, z = cs2cs.TransformPoint(xmin, ymin)
+            maxlon, maxlat, z = cs2cs.TransformPoint(xmax, ymax)
+            
+            try:
+                handle, filename = mkstemp(dir=self.tmpdir, prefix='render-area-provider-', suffix='.tif')
+                close(handle)
+            
+                ds_area = driver.Create(filename, width+2, height+2, 1, gdal.GDT_Float32)
+                ds_area.SetGeoTransform(buffered_xform)
+                ds_area.SetProjection(area_wkt)
+                
+                ds_args = minlon, minlat, maxlon, maxlat, self.source_dir
+                
+                for ds_dem in module.datasources(*ds_args):
+                
+                    # estimate the raster density across source DEM and output
+                    dem_samples = (maxlon - minlon) / ds_dem.GetGeoTransform()[1]
+                    area_pixels = (xmax - xmin) / ds_area.GetGeoTransform()[1]
+                    
+                    if dem_samples > area_pixels:
+                        # cubic looks better squeezing down
+                        resample = gdal.GRA_Cubic
+                    else:
+                        # cubic spline looks better stretching out
+                        resample = gdal.GRA_CubicSpline
+
+                    gdal.ReprojectImage(ds_dem, ds_area, ds_dem.GetProjection(), ds_area.GetProjection(), resample)
+                    ds_dem.FlushCache()
+                
+                if proportion == 1:
+                    elevation = ds_area.ReadAsArray()
+                else:
+                    elevation += ds_area.ReadAsArray() * proportion
+
+                ds_area.FlushCache()
+
+            finally:
+                unlink(filename)
+        
+        #
+        # Calculate and save slope and aspect.
+        #
+        
+        slope, aspect = calculate_slope_aspect(elevation, xres, yres)
+        
+        tile_xform = xmin, xres, 0, ymax, 0, yres
+        
+        return SlopeAndAspect(self.tmpdir, slope, aspect, area_wkt, tile_xform)
+
+class SlopeAndAspect:
+    """ TileStache response object with PIL-like save() and crop() methods.
+    
+        This object knows only how to save two-band 8-bit GeoTIFFs.
+        
+        See http://tilestache.org/doc/#custom-providers for information
+        on how the SlopeAndAspect object interacts with TileStache.
+    """
+    def __init__(self, tmpdir, slope, aspect, wkt, xform):
+        """ Instantiate with array of slope and aspect, and minimal geographic information.
+        """
+        self.tmpdir = tmpdir
+        
+        self.slope = slope
+        self.aspect = aspect
+        
+        self.w, self.h = self.slope.shape
+
+        self.wkt = wkt
+        self.xform = xform
+    
+    def save(self, output, format):
+        """ Save a two-band GeoTIFF to output file-like object.
+        """
+        assert format == 'TIFF'
+        
+        try:
+            handle, filename = mkstemp(dir=self.tmpdir, prefix='slope-aspect-', suffix='.tif')
+            close(handle)
+            
+            driver = gdal.GetDriverByName('GTiff')
+            gtiff_options = ['COMPRESS=JPEG', 'JPEG_QUALITY=95', 'INTERLEAVE=BAND']
+            ds_both = driver.Create(filename, self.w, self.h, 2, gdal.GDT_Byte, gtiff_options)
+            
+            ds_both.SetGeoTransform(self.xform)
+            ds_both.SetProjection(self.wkt)
+            
+            band_slope = ds_both.GetRasterBand(1)
+            band_slope.SetRasterColorInterpretation(gdal.GCI_Undefined)
+            band_slope.WriteRaster(0, 0, self.w, self.h, slope2bytes(self.slope).tostring())
+            
+            band_aspect = ds_both.GetRasterBand(2)
+            band_aspect.SetRasterColorInterpretation(gdal.GCI_Undefined)
+            band_aspect.WriteRaster(0, 0, self.w, self.h, aspect2bytes(self.aspect).tostring())
+            
+            ds_both.FlushCache()
+            output.write(open(filename, 'r').read())
+        
+        finally:
+            unlink(filename)
+    
+    def crop(self, box):
+        """ Returns a rectangular region from the current image.
+        
+            Box is a 4-tuple with left, upper, right, and lower pixels.
+            Not yet implemented!
+        """
+        raise NotImplementedError()
+
+def choose_providers(zoom):
+    """ Return a list of data sources and proportions for given zoom level.
+    
+        Each data source is a module such as SRTM1 or SRTM3, and the proportions
+        must all add up to one. Return list has either one or two items.
+    """
+    if zoom <= SRTM3.ideal_zoom:
+        return [(SRTM3, 1)]
+
+    elif SRTM3.ideal_zoom < zoom and zoom < SRTM1.ideal_zoom:
+        #bottom, top = SRTM3, SRTM1 # SRTM1 looks terrible
+        bottom, top = SRTM3, NED10m
+
+    elif zoom == SRTM1.ideal_zoom:
+        #return [(SRTM1, 1)] # SRTM1 looks terrible
+        bottom, top = SRTM3, NED10m
+
+    elif SRTM1.ideal_zoom < zoom and zoom < NED10m.ideal_zoom:
+        #bottom, top = SRTM1, NED10m # SRTM1 looks terrible
+        bottom, top = SRTM3, NED10m
+
+    elif zoom >= NED10m.ideal_zoom:
+        return [(NED10m, 1)]
+
+    difference = float(top.ideal_zoom) - float(bottom.ideal_zoom)
+    proportion = 1. - (zoom - float(bottom.ideal_zoom)) / difference
+
+    return [(bottom, proportion), (top, 1 - proportion)]
+
+def slope2bytes(slope):
+    """ Convert slope from floating point to 8-bit.
+    
+        Slope given in radians, from 0 for sheer face to pi/2 for flat ground.
+    """
+    return (0xFF * numpy.sin(slope + pi/2)).astype(numpy.uint8)
+
+def aspect2bytes(aspect):
+    """ Convert aspect from floating point to 8-bit.
+    
+        Aspect given in radians, counterclockwise from -pi at north around to pi.
+    """
+    return (0xFF * (aspect/pi + 1)/2).astype(numpy.uint8)
+
+def bytes2slope(bytes):
+    """ Convert slope from 8-bit to floating point.
+    
+        Slope returned in radians, from 0 for sheer face to pi/2 for flat ground.
+    """
+    return pi/2 - numpy.arcsin(bytes.astype(numpy.float32) / 0xFF)
+
+def bytes2aspect(bytes):
+    """ Convert aspect from 8-bit to floating point.
+    
+        Aspect returned in radians, counterclockwise from -pi at north around to pi.
+    """
+    return (2 * bytes.astype(numpy.float32)/0xFF - 1) * pi
+
+def calculate_slope_aspect(elevation, xres, yres, z=1.0):
+    """ Return a pair of arrays 2 pixels smaller than the input elevation array.
+    
+        Slope is returned in radians, from 0 for sheer face to pi/2 for
+        flat ground. Aspect is returned in radians, counterclockwise from -pi
+        at north around to pi.
+        
+        Logic here is borrowed from hillshade.cpp:
+          http://www.perrygeo.net/wordpress/?p=7
+    """
+    width, height = elevation.shape[0] - 2, elevation.shape[1] - 2
+    
+    window = [z * elevation[row:(row + height), col:(col + width)]
+              for (row, col)
+              in product(range(3), range(3))]
+    
+    x = ((window[0] + window[3] + window[3] + window[6]) \
+       - (window[2] + window[5] + window[5] + window[8])) \
+      / (8.0 * xres);
+    
+    y = ((window[6] + window[7] + window[7] + window[8]) \
+       - (window[0] + window[1] + window[1] + window[2])) \
+      / (8.0 * yres);
+
+    # in radians, from 0 to pi/2
+    slope = pi/2 - numpy.arctan(numpy.sqrt(x*x + y*y))
+    
+    # in radians counterclockwise, from -pi at north back to pi
+    aspect = numpy.arctan2(x, y)
+    
+    return slope, aspect
