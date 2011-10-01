c0412b925830bab8294aa1e1d22e75bd334d24c1
Michal Migurski <migurski@domU-12-31-39-09-1D-E8.compute-1.internal>
2011-10-01 20:59:02 +0000

    Started a basic setup file with version number

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+version = open('VERSION', 'r').read().strip()
+
+setup(name='Hillup',
+      version=version,
+      description='Retrieves and prepares digital elevation data for rendering as map tiles.',
+      author='Michal Migurski',
+      author_email='mike@stamen.com',
+      url='https://github.com/migurski',
+      requires=['ModestMaps','PIL','numpy'],
+      packages=['Hillup'],
+      scripts=[],
+      download_url='https://github.com/downloads/migurski' % locals(),
+      license='BSD')
