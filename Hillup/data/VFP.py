25b0d9d7ed1e6ca1fe66273c7b5e442bf17a412f
migurski <migurski@brillo.(none)>
2012-08-12 19:19:24 -0700

    Removed SRTM3 compositing from VFP module

--- Hillup/data/VFP.py
+++ Hillup/data/VFP.py
@@ -3,7 +3,6 @@ from urlparse import urlparse, urljoin
 from os import unlink, close, write, mkdir, chmod, makedirs
 from os.path import basename, exists, isdir, join
 from httplib import HTTPConnection
-from subprocess import Popen, PIPE
 from tempfile import mkstemp
 from zipfile import ZipFile
 from hashlib import md5
@@ -12,7 +11,7 @@ from .SRTM3 import sref, quads, filename, datasource as srtm3_datasource
 
 from osgeo import gdal
 
-def vfp_datasource(lat, lon, source_dir):
+def datasource(lat, lon, source_dir):
     """
     """
     fmt = 'http://viewfinderpanos-index.herokuapp.com/index.php/%s.hgt'
@@ -101,67 +100,6 @@ def vfp_datasource(lat, lon, source_dir):
     
     finally:
         unlink(zip_path)
-
-    #
-    # The file better exist locally now
-    #
-    return gdal.Open(dem_path, gdal.GA_ReadOnly)
-
-def datasource(lat, lon, source_dir):
-    """ Return a gdal datasource for a VFP lat, lon corner.
-    
-        If it doesn't already exist locally in source_dir, grab a new one.
-    """
-    #
-    # Create a local filepath
-    #
-    dem_dir = md5('Viewfinder Panoramas + SRTM %s.hgt' % filename(lat, lon)).hexdigest()[:3]
-    dem_dir = join(source_dir, dem_dir)
-    
-    dem_path = join(dem_dir, 'VFP-SRTM3-%s.hgt' % filename(lat, lon))
-    dem_none = dem_path[:-4]+'.404'
-    
-    #
-    # Check if the file exists locally
-    #
-    if exists(dem_path):
-        return gdal.Open(dem_path, gdal.GA_ReadOnly)
-
-    if exists(dem_none):
-        return None
-
-    if not exists(dem_dir):
-        makedirs(dem_dir)
-        chmod(dem_dir, 0777)
-    
-    assert isdir(dem_dir)
-    
-    #
-    # Combine the two datasources
-    #
-    vfp_ds = vfp_datasource(lat, lon, source_dir)
-    srtm3_ds = srtm3_datasource(lat, lon, source_dir)
-    
-    if vfp_ds is None and srtm3_ds is None:
-        return None
-    
-    if vfp_ds is None:
-        return srtm3_ds
-    
-    if srtm3_ds is None:
-        return vfp_ds
-    
-    vfp_path, srtm3_path = vfp_ds.GetFileList()[0], srtm3_ds.GetFileList()[0]
-    
-    commands = [['gdalwarp', '-ot', 'Float32', '-co', 'COMPRESS=LZW', srtm3_path, dem_path],
-                ['gdalwarp', '-r', 'cubic', vfp_path, dem_path],
-                ['gdalwarp', srtm3_path, dem_path]]
-    
-    for cmd in commands:
-        cmd = Popen(cmd, stdout=PIPE, stderr=PIPE)
-        cmd.wait()
-        
-        assert cmd.returncode == 0
 
     #
     # The file better exist locally now
